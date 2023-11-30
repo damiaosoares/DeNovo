8a31b6541475dcc7bb79813ae9ba0b342136aec2
damiaosoares <damiao.soares@gmail.com>
2023-11-29 21:39:40 -0300

    desafio dois

---
+++ Desafio 2.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    var mensagem = "O Herói tem um saldo de " + saldoVitorias + " está no nível de " + nivel;
+    return mensagem;
+}
+
+let vitorias = 30;
+let derrotas = 10;
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
+
+let resultadoDois = calcularNivel(30,20);
+console.log(resultadoDois);
